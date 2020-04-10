09d87f00caf6594e76678376d5798c040e794678
afolabi seyi <61024160+samade4love@users.noreply.github.com>
2020-04-10 02:10:52 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-Ninjas></app-Ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-Ninjas': Ninjas
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
